1c99067b2f1c09d348955ec35f2111c5879b650f
Ruthwikreddyn3005 <ruthwikreddyn@gmail.com>
2026-03-12 21:22:26 -0400

    Add SMTP startup verification to debug email issues

--- splitit/server/server.js
+++ splitit/server/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import app from './src/app.js';
 import { connectDB } from './src/config/db.js';
+import { transporter } from './src/config/email.js';
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +9,16 @@ console.log('Starting server...');
 console.log('NODE_ENV:', process.env.NODE_ENV);
 console.log('MONGODB_URI set:', !!process.env.MONGODB_URI);
 console.log('JWT_ACCESS_SECRET set:', !!process.env.JWT_ACCESS_SECRET);
+console.log('SMTP_USER:', process.env.SMTP_USER || '(not set)');
+console.log('SMTP_PASS set:', !!process.env.SMTP_PASS);
+
+transporter.verify((err) => {
+  if (err) {
+    console.error('SMTP connection FAILED:', err.message);
+  } else {
+    console.log('SMTP connection OK - emails ready to send');
+  }
+});
 
 connectDB()
   .then(() => {
